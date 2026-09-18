ceaa113b2f01ec200ab997a58f2eddaf55c8e88e
abbnapuram-manoj <manojabbnapuram@gmail.com>
2026-09-18 10:14:31 +0530

    Add mongoose connection and update emp routes

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const hrRoutes = require('./routes/hr_routes');
-const empRoutes = require('./routes/emp_routes');
+let express = require('express');
+let mongoose = require('mongoose');
 
-const app = express();
+let hrRoutes = require('./routes/hr_routes');
+let empRoutes = require('./routes/emp_routes');
 
+let app = express();
+
+// Required to parse incoming JSON bodies
 app.use(express.json());
 
-// Mount routers
+// MongoDB connection using 127.0.0.1 to avoid Node localhost resolution issues
+mongoose.connect('mongodb://127.0.0.1:27017/hrmanagement')
+    .then(() => console.log("Connected to MongoDB: hrmanagement"))
+    .catch((err) => console.log("DB connection error:", err));
+
+// Modular routes
 app.use('/api/hr', hrRoutes);
 app.use('/api/emp', empRoutes);
 
-const PORT = 3000;
+let PORT = 3000;
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 });
--- routes/emp_routes.js
+++ routes/emp_routes.js
@@ -1,14 +1,52 @@
-const express = require('express');
-const router = express.Router();
+//old code dont include this in the new code    
+// const express = require('express');
+// const router = express.Router();
 
-// GET /api/emp/profile
-router.get('/profile', (req, res) => {
-    res.send('employee profile page called');
+// // GET /api/emp/profile
+// router.get('/profile', (req, res) => {
+//     res.send('employee profile page called');
+// });
+
+// // GET /api/emp/attendance
+// router.get('/attendance', (req, res) => {
+//     res.send('employee attendance page called');
+// });
+
+// module.exports = router;
+
+
+
+
+
+//new code
+let express = require('express');
+
+let router = express.Router();
+
+// POST: /api/emp/register
+router.post('/register', (req, res) => {
+    let data = req.body;
+    res.send(data.name);
 });
 
-// GET /api/emp/attendance
-router.get('/attendance', (req, res) => {
-    res.send('employee attendance page called');
+// POST: /api/emp/login
+router.post('/login', (req, res) => {
+    res.send('login route called');
+});
+
+// GET: /api/emp/viewtasks
+router.get('/viewtasks', (req, res) => {
+    res.send('view tasks page called');
+});
+
+// GET: /api/emp/viewtodo
+router.get('/viewtodo', (req, res) => {
+    res.send('view todo page called');
+});
+
+// PUT: /api/emp/updateprofile
+router.put('/updateprofile', (req, res) => {
+    res.send('update profile page called');
 });
 
 module.exports = router;
